5888469b91e4502f203ec3de825f85858ecb554d
patofb <spfautschb@gmail.com>
2025-09-01 10:25:17 -0600

    manifest update

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,7 @@
 {
     'name': 'Real Estate',
     'version': '18.0.1.0',
-    'license': 'OPL-1'
+    'license': 'OPL-1',
+    'author': 'PatoFb',
+    'description': 'Test module for Odoo Masterclass'
 }
