fde2fb5758dd63b7aa3d2c90d8a15da62e6bbb88
patofb <spfautschb@gmail.com>
2025-09-01 10:31:01 -0600

    category and base dependency added

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'Real Estate',
     'version': '18.0.1.0',
+    'depends': ['base'],
     'license': 'OPL-1',
     'author': 'PatoFb',
     'description': 'Test module for Odoo Masterclass'
