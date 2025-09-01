e26f125c43ccb945e1f6825546da6a061ae04044
PatoFb <42942085+PatoFb@users.noreply.github.com>
2025-09-01 17:59:10 -0600

    DELETE

--- estate/models/estate_property_tag.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class EstatePropertyTag(models.Model):
-    _name = "estate.property.tag"
-    _description = "Property Tag"
-
-    name = fields.Char('Tag Name', required=True, translate=True)
--- estate/models/estate_property_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Property Type"
-
-    name = fields.Char('Type Name', required=True, translate=True)
