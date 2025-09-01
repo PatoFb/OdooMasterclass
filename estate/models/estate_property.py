62a7732cc2f5bc3c11124ebc0109c3998d98d920
patofb <spfautschb@gmail.com>
2025-09-01 12:30:13 -0600

    retry security data

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,24 +6,7 @@ class EstateProperty(models.Model):
 
     name = fields.Char('Property Name', required=True, translate=True)
     description = fields.Text('Property Description', required=True, translate=True)
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float('Expected price $', required=True)
-    selling_price = fields.Float('Selling price $', required=True)
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean(default=False)
-    garden = fields.Boolean(default=False)
-    garden_area = fields.Float()
-    garden_orientation = fields.Selection(
-        selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West'),
-        ]
-    )
+    price = fields.Float('Price $', required=True)
     active = fields.Boolean('Active', default=True)
     address = fields.Text('Property Address', required=True)
     type = fields.Selection(
@@ -34,5 +17,5 @@ class EstateProperty(models.Model):
     )
 
     _sql_constraints = [
-        ('check_price', 'CHECK(expected_price >= 0)', 'The expected price can\'t be negative.'),
+        ('check_price', 'CHECK(price >= 0)', 'The price can\'t be negative.'),
     ]
