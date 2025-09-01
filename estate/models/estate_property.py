9ab05e417243014faa168238f2531989bcaf553a
patofb <spfautschb@gmail.com>
2025-09-01 17:10:36 -0600

    one2many view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -37,5 +37,5 @@ class EstateProperty(models.Model):
         ], default='new', copy=False
     )
     property_type_id = fields.Many2one('estate.property.type', ondelete='set null')
-    partner_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    user_id = fields.Many2one("res.user", string="Salesperson", default=lambda self: self.env.user)
+    partner_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+    user_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
