b05dda403bbb854bb249d636236b3b71807867f1
patofb <spfautschb@gmail.com>
2025-09-01 11:14:02 -0600

    estate property model added

---
+++ estate/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class Property(models.Model):
+    _name = "property"
+    _description = "Property Description"
+
+    name = fields.Char('Property Name', required=True, translate=True)
+    description = fields.Text('Property Description', required=True, translate=True)
+    price = fields.Float('Price $', required=True)
+    active = fields.Boolean('Active', default=True)
+    address = fields.Text('Property Address', required=True)
+    type = fields.Selection(
+        selection=[
+         ("apartment", "Property is an apartment"),
+         ("house", "Property is a house"),
+      ],
+    )
+
+    _sql_constraints = [
+        ('check_price', 'CHECK(price >= 0)', 'The price can\'t be negative.'),
+    ]
