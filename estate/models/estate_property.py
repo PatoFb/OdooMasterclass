d39b761f702aa2543a8dd26aabd590282939b9ed
patofb <spfautschb@gmail.com>
2025-09-01 12:14:00 -0600

    modified fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,7 +6,24 @@ class EstateProperty(models.Model):
 
     name = fields.Char('Property Name', required=True, translate=True)
     description = fields.Text('Property Description', required=True, translate=True)
-    price = fields.Float('Price $', required=True)
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float('Expected price $', required=True)
+    selling_price = fields.Float('Selling price $', required=True)
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean(default=False)
+    garden = fields.Boolean(default=False)
+    garden_area = fields.Float()
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ]
+    )
     active = fields.Boolean('Active', default=True)
     address = fields.Text('Property Address', required=True)
     type = fields.Selection(
