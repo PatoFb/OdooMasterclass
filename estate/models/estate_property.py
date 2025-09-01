e24ff4bc184b69316c12ffade21572328e03a36e
patofb <spfautschb@gmail.com>
2025-09-01 17:18:31 -0600

    many2many view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -39,3 +39,4 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one('estate.property.type', ondelete='set null')
     partner_id = fields.Many2one('res.partner', string="Buyer", copy=False)
     user_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
