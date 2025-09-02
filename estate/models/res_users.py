747bf25dad02bf60af47f878b1a3ef5f269df15c
PatoFb <42942085+PatoFb@users.noreply.github.com>
2025-09-02 17:34:32 -0600

    deleted everything

--- estate/models/res_users.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class ResUsers(models.Model):
-   _inherit = "res.users"
-
-   property_ids = fields.One2many('estate.property', inverse_name='user_id',string='Users',domain=[('state', 'in', ('new', 'offer_received'))])
