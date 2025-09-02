d83104a7317b1dce29704da65a90faef7e73d86e
patofb <spfautschb@gmail.com>
2025-09-02 17:22:57 -0600

    added colors and stuff

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
    _inherit = "res.users"
 
-   property_ids = fields.One2many('estate.property', inverse_name='user_id',string='Users')
+   property_ids = fields.One2many('estate.property', inverse_name='user_id',string='Users',domain=[('state', 'in', ('new', 'offer_received'))])
